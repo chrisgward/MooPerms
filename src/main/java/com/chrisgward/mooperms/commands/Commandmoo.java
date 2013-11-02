7898df556a838617bb95ce69b2e763adbb90103c
Chris Ward <chris@chrisgward.com>
2013-11-03 01:41:30 +1100

    Add copyright to new classes, remove IntelliJ's pesky message

--- src/main/java/com/chrisgward/mooperms/commands/Commandmoo.java
+++ src/main/java/com/chrisgward/mooperms/commands/Commandmoo.java
@@ -1,15 +1,23 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.chrisgward.mooperms.commands;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
-/**
- * Created with IntelliJ IDEA.
- * User: chris
- * Date: 3/11/13
- * Time: 1:15 AM
- * To change this template use File | Settings | File Templates.
- */
 public class Commandmoo extends AbstractCommand {
 	@Override
 	public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
